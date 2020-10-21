6a0f2d3a3399984094834ff1470146498f2ae870
Vec7or <carlo@kirchmeier.co>
2020-10-21 10:53:19 +0200

    #3 Add package.json to build for express

---
+++ express-package.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const fs = require("fs");
+
+let rawdata = fs.readFileSync("package.json");
+let packages = JSON.parse(rawdata);
+delete packages.devDependencies;
+delete packages.scripts;
+fs.writeFileSync("dist/package.json", JSON.stringify(packages));
