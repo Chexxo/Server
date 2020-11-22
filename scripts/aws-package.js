384e97e1666f2faffd9a92f5b214e43f667c1c80
Vec7or <carlo@kirchmeier.co>
2020-11-22 18:04:53 +0100

    #20 minor improvements

--- scripts/aws-package.js
+++ scripts/aws-package.js
@@ -2,4 +2,6 @@
 
 const fs = require("fs");
 fs.renameSync("dist/index_aws.js", "dist/index.js")
-
+fs.unlinkSync("dist/api/ExpressAPIProvider.js")
+fs.unlinkSync("dist/logger/ExpressPersistenceManager.js")
+fs.unlinkSync("dist/logger/ExpressPersistenceManagerConfig.js")
