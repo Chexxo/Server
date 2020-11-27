6d6525a748aad307852e85e2f12a798623ab8595
Vec7or <carlo@kirchmeier.co>
2020-11-27 12:32:14 +0100

    #23 Implement custom port.

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,17 @@ import { ExpressPersistenceManagerConfig } from "./logger/ExpressPersistenceMana
 import { ChexxoServer } from "./server/ChexxoServer";
 import { Logger } from "./shared/logger/Logger";
 
+let port = Number(process.env.PORT);
+if (isNaN(port)) {
+  port = 3000;
+}
+
 const logger = new Logger(
   new ExpressPersistenceManager(new ExpressPersistenceManagerConfig())
 );
 
 const server = new ChexxoServer(
-  new ExpressAPIProvider(),
+  new ExpressAPIProvider(port),
   new CertificateProvider(),
   logger
 );
